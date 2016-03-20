f213a95414fc89d0cf1ce37c664f132031b1f7b0
Luke Bayes <lukebayes@gmail.com>
2016-03-20 12:25:29 -0700

    Working on an exploration

---
+++ src/curryup.js
@@ -0,0 +1,56 @@
+
+var capitalize = function(str) {
+  var parts = str.split('');
+  parts[0] = parts[0].toUpperCase();
+  return parts.join('');
+};
+
+
+var withParams = function(Constr, params) {
+  var values = {};
+
+  function buildParamValues() {
+  };
+
+  function result(var_args) {
+    // TODO(lbayes): Figure out how to call an ES6 constr with dynamic args
+    return new Constr();
+  };
+
+  /*
+  var result = function(var_args) {
+    console.log('values:', values);
+
+    var instance = new Constr();
+
+
+    return instance;
+  };
+  */
+
+  if (params && params.length > 0) {
+    params.forEach(function(param) {
+      console.log('yo:', capitalize(param));
+      result['with' + capitalize(param)] = function(value) {
+        values[param] = value;
+        return result;
+      };
+    });
+  }
+
+  result.build = function() {
+    return new result();
+  };
+
+  return result;
+};
+
+module.exports = {
+  constructor: function(Constr, params) {
+    return withParams(Constr, params);
+  },
+
+  callable: function(callable) {
+  }
+};
+
